9d247d0bdc85889e5d4a5e0cd6889a7aa5cf3b17
AGonzales <gonzalesjoshdre@gmail.com>
2024-02-03 16:02:36 +0800

    slight changes for Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="flex flex-col gap-5">
+    <div className="flex flex-col justify-center items-center gap-5">
         <span className="">{import.meta.env.VITE_API_URL}</span>
       <span className="text-3xl font-bold">TEST WORLD!</span>
     </div>
